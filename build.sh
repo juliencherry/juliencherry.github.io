128481611cf7fd04ef00d49897e305c1f3fd9a50
Julien Cherry <me@juliencherry.net>
2019-06-07 02:43:44 -0400

    Accept output directory as first argument to build script

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env sh
 
-preprocess_html() {
-	dir=$(dirname $1)
-	mustache $dir/mustache.json $dir/index.mustache > $dir/index.html
-	echo "Generated $dir/index.html"
+if [ $# -eq 0 ]; then
+	export OUT_DIR=.
+else
+	export OUT_DIR=$1
+fi
+
+preprocess_html_file() {
+	html_dir=$(dirname $1)
+	out_path="$(pwd)/$OUT_DIR/$html_dir"
+	out_filepath="$out_path/index.html"
+
+	mkdir -p $out_path && touch "$out_filepath"
+	mustache "$html_dir/mustache.json" "$html_dir/index.mustache" > $out_filepath
+	echo "Generated $out_filepath"
 }
 
-export -f preprocess_html
-find . -name "mustache.json" -exec bash -c 'preprocess_html "$0"' {} \;
+export -f preprocess_html_file
+find . -name "mustache.json" -exec bash -c 'preprocess_html_file "$0"' {} \;
 
-sass scss:css
+sass scss:$OUT_DIR/css
 echo "Generated CSS files"
