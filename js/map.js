3b998cc35ad8aca87302fd2fdaea0736225a3c5e
Julien Cherry <me@juliencherry.net>
2020-05-02 03:49:37 -0400

    Migrate to modern Mapbox Static Tiles API

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@ map.addControl(L.control.attribution({
     prefix: "",
 }))
 
-var url ='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}';
+var url ='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
 var attribution = "Made with <a href='https://leafletjs.com/'>Leaflet</a>";
 attribution += " | Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors";
 attribution += " | Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>";
@@ -19,10 +19,12 @@ attribution += " | Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>";
 var imagery = new L.TileLayer(url, {
     accessToken: 'pk.eyJ1IjoianVsaWVuY2hlcnJ5IiwiYSI6ImNqbjhxM3F2czA0eWIza3J5YWY5YWQ3aG8ifQ.BiSRVINfh-aMDu4764_C7A',
     attribution: attribution,
-    id: 'mapbox.streets',
-
+    detectRetina: true,
+    id: 'mapbox/streets-v11',
+    maxZoom: 14,
     minZoom: 1,
-    maxZoom: 13,
+    tileSize: 512,
+    zoomOffset: -1,
 });
 
 map.setView(new L.LatLng(42.3390559, -71.0897654), 13);
