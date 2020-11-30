28fd3c7b566c8c9ecba44bb71e50be28c981b074
Julien Cherry <me@juliencherry.net>
2020-11-29 22:39:25 -0500

    Reformat JavaScript files

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict'
 
 $(document).ready(function() {
-    $("main").flowtype({
-        fontRatio: 30,
-        maxFont: 25,
-        minFont: 20
-    });
+	$('main').flowtype({
+		fontRatio: 30,
+		maxFont: 25,
+		minFont: 20
+	})
 
-    $("a").not("a[href^='https://']")
-          .not("a[href^='mailto:']")
-          .not("a[href^='/']")
-          .not("a[href^='#']")
-          .addClass("unsafe-link")
-          .prepend("<i class='fas fa-unlock-alt fa-sm'></i>");
-});
+	$('a').not('a[href^="https://"]')
+		.not('a[href^="mailto:"]')
+		.not('a[href^="/"]')
+		.not('a[href^="#"]')
+		.addClass('unsafe-link')
+		.prepend('<i class="fas fa-unlock-alt fa-sm"></i>')
+})
